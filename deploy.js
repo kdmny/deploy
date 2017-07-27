c8268313514c723828d95bf309c45f7f37c572e8
Leonardo Kewitz <leokewitz@gmail.com>
2017-07-27 12:07:59 -0300

    fix: GCLOUD deploy error

--- deploy.js
+++ deploy.js
@@ -3,6 +3,7 @@
 
 require('dotenv').config()
 const shell = require('shelljs')
+const { spawn } = require('child_process')
 
 const { GCLOUD_PROJECT } = process.env
 const usage = `Usage: deploy branch
@@ -17,9 +18,9 @@ const verifyDependency = (condition, msg) => {
   }
 }
 
-const exec = cmd => {
-  console.log(`> ${cmd}`)
-  return shell.exec(cmd, { maxBuffer: 1024 * 1024 })
+const exec = (cmd, args) => {
+  console.log(`> ${cmd} ${args.join(' ')}`)
+  return spawn(cmd, args, { stdio: 'inherit' })
 }
 
 const [ branch, ] = process.argv.slice(2)
@@ -32,16 +33,16 @@ if (branch) {
   const bucket = `gs://${GCLOUD_PROJECT}-backend`
   const storage = `storage/${branch}`
   if (branch === 'setup') {
-    exec(`gsutil mb -p ${GCLOUD_PROJECT} ${bucket}`)
+    exec('gsutil', ['mb', '-p', GCLOUD_PROJECT, bucket])
   } else {
     verifyDependency(shell.test('-e', `${branch}.yaml`), `Sorry, I couldn't find "${branch}.yaml"`)
     if(shell.test('-d', storage)) {
       console.log('Syncing static files...')
-      exec(`gsutil -m rsync -d -r ${storage} ${bucket}/${branch}`)
+      exec('gsutil', ['-m', 'rsync', '-d', '-r', storage, `${bucket}/${branch}`])
     } else console.log(`${storage} not found, skipping static files sync.`)
 
     console.log(`Deploying ${branch} to GAE...`)
-    exec(`gcloud app deploy ${branch}.yaml --project=${GCLOUD_PROJECT} -q`)
+    exec('gcloud', ['app', 'deploy', `${branch}.yaml`, `--project=${GCLOUD_PROJECT}`, '-q'])
   }
 }
 else console.log(usage)

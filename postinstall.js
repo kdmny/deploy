f5659335a43f564f95f806c2c8f31ed2079c96ba
Gunar C. Gessner <gunar@gunargessner.com>
2017-08-08 16:34:28 -0300

    fix: shell.cd was not working

--- postinstall.js
+++ postinstall.js
@@ -9,9 +9,6 @@ const exec = (cmd, args) => {
   return spawn(cmd, args, { stdio: 'inherit' })
 }
 
-// change directory out of node_modules (in order to find .env)
-shell.cd('../..')
-
 const isOnCloud = Boolean(process.env.GAE_SERVICE)
 if (isOnCloud) {
   exec('node_modules/.bin/sync')
